321fabd093a42def07462c5b5bd0260f7c13eb30
zhoulei <leochou73@163.com>
2021-06-21 21:05:52 +0800

    hot-fix 4 commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,8 +2,8 @@ public class GitTest {
     private int id;
     private int id2;
     private int id4;
-    private int id3;
 
-    private String str;
-
+    public void ShowHello(){
+        System.out.println("hello!");
+    }
 }
